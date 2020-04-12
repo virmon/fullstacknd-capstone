9fbd4eaa337e29eb538b0bc3a78747f34a105019
virmon <virmontumulak@gmail.com>
2020-04-11 21:55:47 -0500

    chore: add setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,7 @@
-
+export FLASK_APP=app.py
+export FLASK_ENV=development
+export AUTH0_DOMAIN='fullstacknd-capstone.auth0.com'
+export ALGORITHMS=['RS256']
+export API_AUDIENCE='movie'
+export CLIENT_ID='LNlBEBoUOOHQgDeh51TVaYlogoZT8FAG'
+export DATABASE_URL='postgresql://postgres:psql@localhost:5432/casting'
